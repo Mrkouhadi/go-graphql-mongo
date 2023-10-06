858bf6ac3597db1900d5f5969385976af16b8b08
mrkouhadi <teacherbryan@outlook.com>
2023-10-06 15:38:36 +0800

    ref: some refactering....

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"go-gql-mongo/graph"
 
@@ -11,19 +10,19 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-const defaultPort = "8080"
-
+// FIXME: enable CORS so we can access it using javascript
+// chi router + graphql : https://blog.devgenius.io/intergrating-graphql-with-golang-the-right-way-d6a27bf4cbf7
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-
+	port := "8080"
+	var srv = handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
-
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
+
+/// enabling CORS:
+// func enableCors(w *http.ResponseWriter) {
+// 	(*w).Header().Set("Access-Control-Allow-Origin", "*") // this means open all possible origins
+// 	// (*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // open to only http://localhost:3000
+// }
